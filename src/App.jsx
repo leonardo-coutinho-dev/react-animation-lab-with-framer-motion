51b77043cf482ddd833d5331c771e84a725c4e50
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-21 23:59:49 +0000

    animation - rotate text 360 deg on hover / whileTap added to the logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <>
-      <AnimatedText01 children={"Hello, world!"} />
       <AnimatedReactLogo />
+
+      <AnimatedText children={"Hello, world!"} />
+      <AnimatedBoxRotate children={"Hello, world!"} />
     </>
   );
 }
@@ -15,6 +17,7 @@ function AnimatedReactLogo() {
   return (
     <motion.div
       whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.8 }}
       className="overflow-hidden absolute top-1 left-1 p-4"
     >
       <motion.img src={logo} width="50px" />
@@ -22,30 +25,49 @@ function AnimatedReactLogo() {
   );
 }
 
-function AnimatedText01({ children }) {
+function AnimatedText({ children }) {
   return (
-    <>
-      <div className="bg-indigo-50 p-8 m-4 border-3 border-sky-300 border-dashed rounded-full">
-        <motion.a
-          className="overflow-hidden relative block"
-          initial="initial"
-          whileHover="hovered"
+    <div className="bg-indigo-50 p-8 m-4 border-3 border-sky-300 border-dashed rounded-full">
+      <motion.a
+        className="overflow-hidden relative block"
+        initial="initial"
+        whileHover="hovered"
+      >
+        <motion.div variants={{ initial: { y: 0 }, hovered: { y: "-100%" } }}>
+          <motion-h1 className="font-mono text-6xl">{children}</motion-h1>
+        </motion.div>
+        <motion.div
+          className="absolute inset-0"
+          variants={{ initial: { y: "100%" }, hovered: { y: 0 } }}
         >
-          <motion.div variants={{ initial: { y: 0 }, hovered: { y: "-100%" } }}>
-            <motion-h1 className="font-mono text-6xl">{children}</motion-h1>
-          </motion.div>
-          <motion.div
-            className="absolute inset-0"
-            variants={{ initial: { y: "100%" }, hovered: { y: 0 } }}
-          >
-            <motion.h1 className="font-mono text-6xl">{children}</motion.h1>
-          </motion.div>
-        </motion.a>
-      </div>
-    </>
+          <motion.h1 className="font-mono text-6xl">{children}</motion.h1>
+        </motion.div>
+      </motion.a>
+    </div>
   );
 }
 
-export { AnimatedReactLogo, AnimatedText01 };
+function AnimatedBoxRotate({ children }) {
+  return (
+    <motion.div
+      className="bg-indigo-50 p-8 m-4 border-3 border-sky-300 border-dashed rounded-full"
+      initial="initial"
+      whileHover="hovered"
+    >
+      <motion.h1
+        variants={{
+          initial: { rotate: "0deg" },
+          hovered: { rotate: "360deg" },
+        }}
+        transition={{ type: "spring" }}
+        className="font-mono text-6xl"
+      >
+        {children}
+      </motion.h1>
+    </motion.div>
+  );
+}
+
+export { AnimatedReactLogo, AnimatedText, AnimatedBoxRotate };
 
 export default App;
