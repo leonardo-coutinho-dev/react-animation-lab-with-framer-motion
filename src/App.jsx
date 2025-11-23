d9f6c4c78ce9111c8eb00ea77c5319cb00fc420a
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-23 11:21:24 +0000

    testing the hide and show animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import logo from "./assets/react-svg-logo.svg";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+
+import { useState } from "react";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center">
       <AnimatedReactLogo />
 
       <AnimatedTextTranslateY children={"Hello, world!"} />
       <AnimatedTextRotate children={"Hello, world!"} />
       <AnimatedTextOpacity children={"Hello, world!"} />
-    </>
+      <AnimatedExitBox />
+    </div>
   );
 }
 
@@ -90,11 +93,69 @@ function AnimatedTextOpacity({ children }) {
   );
 }
 
+function AnimatedExitBox() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <div style={container}>
+      <AnimatePresence initial={false}>
+        {isVisible ? (
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            style={box}
+            key="box"
+          />
+        ) : null}
+      </AnimatePresence>
+      <motion.button
+        style={button}
+        onClick={() => setIsVisible(!isVisible)}
+        whileTap={{ y: 1 }}
+      >
+        {isVisible ? "Hide" : "Show"}
+      </motion.button>
+    </div>
+  );
+}
+
+/**
+ * ==============   Styles   ================
+ */
+
+const container = {
+  display: "flex",
+  flexDirection: "column",
+  width: 100,
+  height: 160,
+  position: "relative",
+};
+
+const box = {
+  width: 100,
+  height: 100,
+  backgroundColor: "#0cdcf7",
+  borderRadius: "10px",
+};
+
+const button = {
+  backgroundColor: "#0cdcf7",
+  borderRadius: "10px",
+  padding: "10px 20px",
+  color: "#0f1115",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  right: 0,
+};
+
 export {
   AnimatedReactLogo,
   AnimatedTextTranslateY,
   AnimatedTextRotate,
   AnimatedTextOpacity,
+  AnimatedExitBox,
 };
 
 export default App;
